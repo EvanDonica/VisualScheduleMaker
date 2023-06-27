3391849bcc53e67008ff584f63f69bfe983eda3a
Evan Donica <cevandonica@gmail.com>
2023-06-27 13:46:24 -0500

    started the server

---
+++ Backend/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(":8080", nil)
+}
